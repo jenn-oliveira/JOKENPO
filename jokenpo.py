a897d4810ea520c69c591d57f2c3cf2a7a103d0d
Jennifer Oliveira <90784809+Bebita13@users.noreply.github.com>
2022-02-09 00:12:40 -0300

    Update jokenpo.py

--- jokenpo.py
+++ jokenpo.py
@@ -4,47 +4,9 @@ import streamlit as st
 
 itens = ('PEDRA', 'PAPEL', 'TESOURA')
 pc = randint(0, 2)
-j1 = st.selectbox ( 
-    "ESCOLHA:"
-    ('0 - PEDRA', '1 - PAPEL', '2 - TESOURA'))
-st.write('Você escolheu:{}'.format(itens[j1]))
-st.spinner('JO')
-time.sleep(1)
-st.spinner('KEN')
-time.sleep(1)
-st.spinner('PO!!!')
-time.sleep(1)
-st.text('-=' * 15)
-st.text('O computador escolheu {}'.format(itens[pc]))
-st.text('-=' * 15)
-if pc == 0:
-    if j1 == 0:
-        st.info('EMPATE!')
-    elif j1 == 1:
-        st.success('VOCÊ VENCEU!')
-        st.balloons()
-    elif j1 == 2:
-        st.error('COMPUTADOR VENCEU!')
-    else:
-        st.warning('JOGADA INVÁLIDA!')
-
-elif pc == 1:
-    if j1 == 0:
-        st.error('COMPUTADOR VENCEU!')
-    elif j1 == 1:
-        st.info('EMPATE')
-    elif j1 == 2:
-        st.success('VOCÊ VENCEU!')
-        st.balloons()
-    else:
-        st.warning('JOGADA INVÁLIDA!')
-
-elif pc == 2:
-    if j1 == 0:
-        st.success('VOCÊ VENCEU!')
-    elif j1 == 1:
-        st.error('COMPUTADOR VENCEU!')
-    elif j1 == 2:
-        st.info('EMPATE')
-    else:
-        st.warning('JOGADA INVÁLIDA!')
+st.text('''ESCOLHA:
+[ 0 ] PEDRA
+[ 1 ] PAPEL
+[ 2 ] TESOURA''')
+j1 = st.text_input('Sua escolha: ')
+st.text('Você escolheu {}'.format(itens[j1]))
