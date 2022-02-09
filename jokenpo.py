51b0aa712175ca13c0367ab3e9170fa797b8c464
Jennifer Oliveira <90784809+Bebita13@users.noreply.github.com>
2022-02-09 00:20:04 -0300

    Update jokenpo.py

--- jokenpo.py
+++ jokenpo.py
@@ -9,4 +9,43 @@ st.text('''ESCOLHA:
 [ 1 ] PAPEL
 [ 2 ] TESOURA''')
 j1 = st.text_input('Sua escolha: ')
-st.write('Você escolheu ", j1)
+st.spinner('JO')
+time.sleep(1)
+st.spinner('KEN')
+time.sleep(1)
+st.spinner('PO!!!')
+time.sleep(1)
+st.text('-=' * 15)
+st.text('O computador escolheu {}'.format(itens[pc]))
+st.text('-=' * 15)
+if pc == 0:
+    if j1 == 0:
+        st.info('EMPATE!')
+    elif j1 == 1:
+        st.success('VOCÊ VENCEU!')
+        st.balloons()
+    elif j1 == 2:
+        st.error('COMPUTADOR VENCEU!')
+    else:
+        st.warning('JOGADA INVÁLIDA!')
+
+elif pc == 1:
+    if j1 == 0:
+        st.error('COMPUTADOR VENCEU!')
+    elif j1 == 1:
+        st.info('EMPATE')
+    elif j1 == 2:
+        st.success('VOCÊ VENCEU!')
+        st.balloons()
+    else:
+        st.warning('JOGADA INVÁLIDA!')
+
+elif pc == 2:
+    if j1 == 0:
+        st.success('VOCÊ VENCEU!')
+    elif j1 == 1:
+        st.error('COMPUTADOR VENCEU!')
+    elif j1 == 2:
+        st.info('EMPATE')
+    else:
+        st.warning('JOGADA INVÁLIDA!')
